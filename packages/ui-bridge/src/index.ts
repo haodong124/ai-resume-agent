b3d81ca38a4ab08b6f311c00340b279f4ff8352e
haodong124 <haodongxu124@gmail.com>
2025-08-30 16:15:24 +0800

    Create index.ts

---
+++ packages/ui-bridge/src/index.ts
@@ -0,0 +1,4 @@
+export { ResumeContentOptimizer } from './ResumeContentOptimizer'
+export { JobMatchAgent } from './JobMatchAgent'
+export { SkillRecommendAgent } from './SkillRecommendAgent'
+export { KeywordExtractor } from './KeywordExtractor'
