eced64d04416c481a12716522c183c5a714da956
haodong124 <haodongxu124@gmail.com>
2025-08-31 11:38:36 +0800

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,15 +1,36 @@
+// apps/web/src/App.tsx - 主应用组件
+// 文件路径: apps/web/src/App.tsx
+
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import WorkspacePage from './pages/WorkspacePage'
+import { Toaster } from 'react-hot-toast'
+import { HomePage } from './pages/HomePage'
+import { EditorPage } from './pages/EditorPage'
+import { JobsMatchPage } from './pages/JobsMatchPage'
+import { SkillRecommender } from './components/SkillRecommender'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/workspace" element={<WorkspacePage />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/jobs-match" element={<JobsMatchPage />} />
+          <Route path="/skills" element={<SkillRecommender />} />
+        </Routes>
+        
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
+      </div>
     </Router>
   )
 }
