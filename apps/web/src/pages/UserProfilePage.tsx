0ff5488d880b3a9acb0a7d5c371b47ce94df7508
haodong124 <haodongxu124@gmail.com>
2025-09-04 15:59:36 +0800

    Create UserProfilePage.tsx

---
+++ apps/web/src/pages/UserProfilePage.tsx
@@ -0,0 +1,13 @@
+// apps/web/src/pages/UserProfilePage.tsx
+import React from 'react'
+import UserProfile from '../components/UserProfile'
+
+const UserProfilePage: React.FC = () => {
+  return (
+    <div className="py-6">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default UserProfilePage
